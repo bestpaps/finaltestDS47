9980c1ea7f63049f3a8f956fc829ac8dfc083389
bestpaps <bestpaps69@gmail.com>
2025-05-28 15:48:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import nltk
 import re
 import os
 
-# Tambahkan path ke data NLTK lokal
 nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
 if nltk_data_path not in nltk.data.path:
     nltk.data.path.append(nltk_data_path)
